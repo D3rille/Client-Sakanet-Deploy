a3b7a81bd781c3b6308cc0955622601a687146ea
Stephanie <stphnencomienda30@gmail.com>
2023-08-21 08:55:54 +0800

    updated product card and grid

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -5,87 +5,89 @@ import {
   CardMedia,
   Typography,
   Button,
-  IconButton,
 } from "@mui/material";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import Link from "next/link"; // Ensure to import the Link component
 
 function ProductCard({ product }) {
-  return (
-    <Card
-      sx={{
-        width: "100%",
-        height: "350px",
-        borderRadius: "12px",
-        mb: 1,
-        boxShadow: 3,
-      }}
-    >
-      <CardMedia
-        sx={{ borderRadius: "10px" }}
-        component="img"
-        alt={product.title}
-        height="200"
-        image={product.img}
-      />
-      <CardContent>
-        <Typography
-          variant="body1"
-          align="right"
-          sx={{ flexGrow: 1, fontWeight: "bold" }}
-        >
-          ₱ {product.price}/kg
-        </Typography>
-        <Typography
-          gutterBottom
-          align="left"
-          sx={{ fontSize: "1.1rem", fontWeight: "bolder" }}
-        >
-          {product.title}
-        </Typography>
-        <Typography
-          color="textSecondary"
-          gutterBottom
-          align="left"
-          sx={{ fontSize: "0.8rem" }}
-        >
-          {product.location}
-        </Typography>
-      </CardContent>
-      <div style={{ display: "flex", width: "100%" }}>
-        <Typography
-          style={{
-            backgroundColor: "#ECEDEC",
-            padding: "10px",
-            color: "#2C2D2D",
-            borderBottomLeftRadius: "12px",
-            width: "50%",
-            fontSize: "0.8rem",
-            margin: 0,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          Stocks: {product.stock} kg
-        </Typography>
-        <Button
-          variant="contained"
-          endIcon={<AddShoppingCartIcon style={{ color: "#C9D5CA" }} />}
-          style={{
-            backgroundColor: "#2F603B",
-            color: "#C9D5CA",
-            width: "50%",
-            fontSize: "0.7rem",
-            borderBottomRightRadius: "12px",
-            margin: 0,
-            borderRadius: 0,
-          }}
-        >
-          Add to Cart
-        </Button>
-      </div>
-    </Card>
-  );
+    return (
+      <Card
+        sx={{
+          width: "100%",
+          height: "350px",
+          borderRadius: "12px",
+          mb: 1,
+          boxShadow: 3,
+        }}
+      >
+        <CardMedia
+          sx={{ borderRadius: "10px" }}
+          component="img"
+          alt={product.title}
+          height="200"
+          image={product.img}
+        />
+        <CardContent>
+          <Typography
+            variant="body1"
+            align="right"
+            sx={{ flexGrow: 1, fontWeight: "bold" }}
+          >
+            ₱ {product.price}/kg
+          </Typography>
+          <Typography
+            gutterBottom
+            align="left"
+            sx={{ fontSize: "1.1rem", fontWeight: "bolder" }}
+          >
+            {product.title}
+          </Typography>
+          <Typography
+            color="textSecondary"
+            gutterBottom
+            align="left"
+            sx={{ fontSize: "0.8rem" }}
+          >
+            {product.location}
+          </Typography>
+        </CardContent>
+        <div style={{ display: "flex", width: "100%" }}>
+          <Typography
+            style={{
+              backgroundColor: "#ECEDEC",
+              padding: "10px",
+              color: "#2C2D2D",
+              borderBottomLeftRadius: "12px",
+              width: "50%",
+              fontSize: "0.8rem",
+              margin: 0,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Stocks: {product.stock} kg
+          </Typography>
+          <Button
+            variant="contained"
+            endIcon={<AddShoppingCartIcon style={{ color: "#C9D5CA" }} />}
+            style={{
+              backgroundColor: "#2F603B",
+              color: "#C9D5CA",
+              width: "50%",
+              fontSize: "0.7rem",
+              borderBottomRightRadius: "12px",
+              margin: 0,
+              borderRadius: 0,
+            }}
+            component={Link} // use the Link as the component for Button
+            href={`/Products/productOverview?productId=${product.id}`}
+          >
+            Add to Cart
+          </Button>
+        </div>
+      </Card>
+    );
 }
 
 export default ProductCard;
